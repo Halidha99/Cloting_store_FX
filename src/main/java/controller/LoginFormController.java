a49e3fef7f671f0630c44c668a1678c51eb0438e
Halidha99 <fathimahalidha94@gmail.com>
2024-10-20 02:06:14 +0530

    login

---
+++ src/main/java/controller/LoginFormController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LoginFormController {
+
+    @FXML
+    private JFXButton btnRegister;
+
+    @FXML
+    private JFXButton btnSignIn;
+
+    @FXML
+    private Label lblShopName;
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    private JFXPasswordField txtxPassword;
+
+    @FXML
+    void LoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegisterOnAction(ActionEvent event) {
+
+    }
+
+}
